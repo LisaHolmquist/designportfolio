f78b8a40b3503cb271309c350fd9ee55c78c96a3
LisaHolmquist <110413449+LisaHolmquist@users.noreply.github.com>
2023-08-16 16:06:51 -0600

    mobile update - test ready

--- UX Design Tab/Amazon Case Study/script.js
+++ UX Design Tab/Amazon Case Study/script.js
@@ -35,6 +35,27 @@ window.addEventListener("scroll", () => {
 
 
 
+const hamburger = document.querySelector(".burger-menu");
+const navbar = document.querySelector(".mob-nav");
+
+hamburger.addEventListener("click", ()=> {
+    navbar.classList.toggle("open");
+});
+
+const hamburgerBtn = document.querySelector('.burger-menu');
+let hamburgerOpen = false;
+
+hamburgerBtn.addEventListener('click', () => {
+    if (!hamburgerOpen) {
+        hamburgerBtn.classList.add('open');
+        hamburgerOpen = true;
+    } else {
+        hamburgerBtn.classList.remove('open');
+        hamburgerOpen = false;
+    }
+});
+
+
 //document.getElementById("backToTop").addEventListener("mouseover", mouseOver);
 //console.log("mouse over");
 //document.getElementById("backToTop").addEventListener("mouseout", mouseOut);
